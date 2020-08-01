2cb6b3f1cdb4f86451d22a0aaedda21c51956d80
borisbrunop <borisbruno88@gmail.com>
2020-08-01 02:36:51 -0400

    ejercicio hecho

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
+	const [state, setState] = useState({ clickedLight: null });
+
+	let redSelected = "";
+	if (state.clickedLight === "red") redSelected = "selected";
+	let yellowSelected = "";
+	if (state.clickedLight === "yellow") yellowSelected = "selected";
+	let greenSelected = "";
+	if (state.clickedLight === "green") greenSelected = "selected";
+
+	console.log(state);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container trafficTop " />
+			<div className="container traffic rounded justify-content-center">
+				<div
+					className={"red light rounded-circle " + redSelected}
+					onClick={() => setState({ clickedLight: "red" })}
+				/>
+				<div
+					className={"yellow light rounded-circle " + yellowSelected}
+					onClick={() => setState({ clickedLight: "yellow" })}
+				/>
+				<div
+					className={"green light rounded-circle " + greenSelected}
+					onClick={() => setState({ clickedLight: "green" })}
+				/>
+			</div>
+		</>
 	);
 }
